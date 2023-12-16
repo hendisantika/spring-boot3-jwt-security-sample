cf77521ca337bb4d5fc2f9dcad65c6d36bba9535
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:51:56 +0700

    Add get in ManagementController🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3jwtsecuritysample/controller/ManagementController.java
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/controller/ManagementController.java
@@ -1,6 +1,9 @@
 package com.hendisantika.springboot3jwtsecuritysample.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +26,25 @@ public class ManagementController {
     public String post() {
         return "POST:: management controller";
     }
+
+    @Operation(
+            description = "Get endpoint for manager",
+            summary = "This is a summary for management get endpoint",
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized / Invalid Token",
+                            responseCode = "403"
+                    )
+            }
+
+    )
+    @GetMapping
+    public String get() {
+        return "GET:: management controller";
+    }
+
 }
