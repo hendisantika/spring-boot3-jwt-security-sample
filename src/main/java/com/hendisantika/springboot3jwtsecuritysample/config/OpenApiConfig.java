c33752a8d954e4eb1e1509c4dc45171cc95ee8d8
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:46:14 +0700

    Add OpenApiConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/config/OpenApiConfig.java
@@ -0,0 +1,64 @@
+package com.hendisantika.springboot3jwtsecuritysample.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:45
+ * To change this template use File | Settings | File Templates.
+ */
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Hendi Santika",
+                        email = "hendisantika@yahoo.co.id",
+                        url = "https://s.id/hendisantika/"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Hendi Santika",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8081"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://www.linkedin.com/id/hendisantika/"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
