f082aed676edcd0051738de705fd103a0f6600b5
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:31:36 +0700

    Add Role Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/entity/Role.java
@@ -0,0 +1,59 @@
+package com.hendisantika.springboot3jwtsecuritysample.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.security.Permission;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:31
+ * To change this template use File | Settings | File Templates.
+ */
+@RequiredArgsConstructor
+public enum Role {
+
+    USER(Collections.emptySet()),
+    ADMIN(
+            Set.of(
+                    Permission.ADMIN_READ,
+                    Permission.ADMIN_UPDATE,
+                    Permission.ADMIN_DELETE,
+                    Permission.ADMIN_CREATE,
+                    Permission.MANAGER_READ,
+                    Permission.MANAGER_UPDATE,
+                    Permission.MANAGER_DELETE,
+                    Permission.MANAGER_CREATE
+            )
+    ),
+    MANAGER(
+            Set.of(
+                    Permission.MANAGER_READ,
+                    Permission.MANAGER_UPDATE,
+                    Permission.MANAGER_DELETE,
+                    Permission.MANAGER_CREATE
+            )
+    );
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+}
