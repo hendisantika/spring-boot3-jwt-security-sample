ee164ea5956d912dbb0f49f3c7d77b9fe10e842e
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:44:38 +0700

    Add logout in LogoutService🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3jwtsecuritysample/service/LogoutService.java
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/service/LogoutService.java
@@ -1,7 +1,14 @@
 package com.hendisantika.springboot3jwtsecuritysample.service;
 
+import com.hendisantika.springboot3jwtsecuritysample.entity.Token;
+import com.hendisantika.springboot3jwtsecuritysample.exception.InvalidTokenException;
+import com.hendisantika.springboot3jwtsecuritysample.exception.TokenNotFoundException;
 import com.hendisantika.springboot3jwtsecuritysample.repository.TokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +27,27 @@ import org.springframework.stereotype.Service;
 public class LogoutService implements LogoutHandler {
 
     private final TokenRepository tokenRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            throw new InvalidTokenException("Token is missing");
+        } else if (!authorization.startsWith("Bearer ")) {
+            throw new InvalidTokenException("Token is invalid");
+        }
+
+        final String jwt = authorization.substring(7);
+
+        Token storedToken = tokenRepository.findByToken(jwt)
+                .orElseThrow(() -> new TokenNotFoundException("Token not found"));
+
+        if (storedToken != null) {
+            storedToken.setIsExpired(true);
+            storedToken.setIsRevoked(true);
+            tokenRepository.save(storedToken);
+            SecurityContextHolder.clearContext();
+        }
+    }
 }
