1a386d0f12f329e725cc6e046c2f5060622c153a
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:58:42 +0700

    Add refreshToken in AuthenticationController🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3jwtsecuritysample/controller/AuthenticationController.java
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/controller/AuthenticationController.java
@@ -4,12 +4,16 @@ import com.hendisantika.springboot3jwtsecuritysample.request.AuthenticationReque
 import com.hendisantika.springboot3jwtsecuritysample.request.RegisterRequest;
 import com.hendisantika.springboot3jwtsecuritysample.response.AuthenticationResponse;
 import com.hendisantika.springboot3jwtsecuritysample.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,5 +40,10 @@ public class AuthenticationController {
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authenticationService.refreshToken(request, response);
     }
 }
