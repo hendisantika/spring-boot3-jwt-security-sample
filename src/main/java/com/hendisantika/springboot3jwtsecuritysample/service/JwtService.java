db4698ec5f614167f47d9a149f84aa12d1a0a89d
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:41:56 +0700

    Add extractExpiration in JwtService🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3jwtsecuritysample/service/JwtService.java
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/service/JwtService.java
@@ -74,4 +74,8 @@ public class JwtService {
     private boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
