8009abdc2d7701f1b1ce0942ab9e6a98549235d0
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:32:09 +0700

    Add Permission Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/entity/Permission.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springboot3jwtsecuritysample.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:31
+ * To change this template use File | Settings | File Templates.
+ */
+@RequiredArgsConstructor
+public enum Permission {
+
+    ADMIN_READ("admin:read"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
+    MANAGER_READ("management:read"),
+    MANAGER_UPDATE("management:update"),
+    MANAGER_CREATE("management:create"),
+    MANAGER_DELETE("management:delete");
+
+    @Getter
+    private final String permission;
+}
--- src/main/java/com/hendisantika/springboot3jwtsecuritysample/entity/Role.java
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/entity/Role.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.security.Permission;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
