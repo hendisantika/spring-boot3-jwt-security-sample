6364ae60e541fea934581d578eb48599d846a1ed
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:27:42 +0700

    Create starter project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/SpringBoot3JwtSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot3jwtsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JwtSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3JwtSecuritySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springboot3jwtsecuritysample/SpringBoot3JwtSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot3jwtsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtSecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
