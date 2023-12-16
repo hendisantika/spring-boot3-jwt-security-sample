bb657f7e5a81802f8fc78408d80451494c94d846
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:58:16 +0700

    Add register in AuthenticationController🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springboot3jwtsecuritysample/controller/AuthenticationController.java
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/controller/AuthenticationController.java
@@ -1,7 +1,12 @@
 package com.hendisantika.springboot3jwtsecuritysample.controller;
 
+import com.hendisantika.springboot3jwtsecuritysample.request.RegisterRequest;
+import com.hendisantika.springboot3jwtsecuritysample.response.AuthenticationResponse;
 import com.hendisantika.springboot3jwtsecuritysample.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
 }
