814e0feaa67c396b520542bda719588869cecc3c
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:57:56 +0700

    Add AuthenticationController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springboot3jwtsecuritysample.controller;
+
+import com.hendisantika.springboot3jwtsecuritysample.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:57
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+}
