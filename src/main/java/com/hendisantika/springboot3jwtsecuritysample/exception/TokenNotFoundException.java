accd221444994770a4611593087ea7f36c87f159
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:30:04 +0700

    Add TokenNotFoundException🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/exception/TokenNotFoundException.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springboot3jwtsecuritysample.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class TokenNotFoundException extends RuntimeException {
+    public TokenNotFoundException(String message) {
+        super(message);
+    }
+}
