cf81e74f83ea970f70c718cf5154356893a63bd7
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:37:30 +0700

    Add RegisterRequest🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/request/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springboot3jwtsecuritysample.request;
+
+import com.hendisantika.springboot3jwtsecuritysample.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
