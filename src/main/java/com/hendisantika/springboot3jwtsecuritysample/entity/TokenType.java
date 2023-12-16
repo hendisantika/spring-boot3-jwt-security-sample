afc39cf39fd1488c76f6da6d8e03e89ce980adc2
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:32:37 +0700

    Add TokenType Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/entity/TokenType.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springboot3jwtsecuritysample.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:32
+ * To change this template use File | Settings | File Templates.
+ */
+public enum TokenType {
+    BEARER
+}
