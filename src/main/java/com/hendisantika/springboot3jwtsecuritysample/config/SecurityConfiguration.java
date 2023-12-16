046b0ffa43eba73e1447bd7ea952c83d0c0a029b
hendisantika <hendisantika@yahoo.co.id>
2023-12-16 08:49:47 +0700

    Add SecurityConfiguration🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springboot3jwtsecuritysample/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springboot3jwtsecuritysample.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/16/23
+ * Time: 08:49
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+}
